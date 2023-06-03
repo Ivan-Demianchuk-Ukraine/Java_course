5e82b69ed41228b89c7fa98ed60ca8e46f013d91
IvanDemianchuk <energy.redler@gmail.com>
2023-06-03 16:38:40 -0400

    ThirdCommit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//4. Повторить задание 2, но выполнить его в отдельных методах и запустить эти методы в методе Main
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        System.out.printf("TestText!");
-        System.out.printf("NewText!");
-        }
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+
+        float e = 5.5f;
+        double g = 6.7;
+        boolean j = true;
+        char h = 7;
+        char z = 9;
+        int k = (int) (b - d);
+        int in = b + a;
+
+
+        System.out.println(b * d);
+        System.out.println(g / c);
+        System.out.println(j);
+        System.out.println(g + e);
+        System.out.println(h);
+        System.out.println(z + h);
+        System.out.println(a != 0);
+        System.out.println(Math.round(g));
+        System.out.println(k);
+        System.out.println(in);
+
     }
+}
